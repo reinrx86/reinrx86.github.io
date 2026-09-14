127399501529eb5a5b56477f6a82a1e5d5a9fafa
reinrx86 <rein.rx86@gmail.com>
2026-09-14 02:30:27 -0300

    Update fanarts.js

--- Museo-Fanarts/fanarts.js
+++ Museo-Fanarts/fanarts.js
@@ -124,11 +124,11 @@ const fanarts = {
         "https://res.cloudinary.com/thlvsuvu/image/upload/f_auto,q_auto,w_2560/v1788595192/koppermaximusarg-1_jwp076.png",
         "https://res.cloudinary.com/thlvsuvu/image/upload/f_auto,q_auto,w_2560/v1788595192/koppermaximusarg-2_okabjg.png",
         "https://res.cloudinary.com/thlvsuvu/image/upload/f_auto,q_auto,w_2560/v1788595197/koppermaximusarg-3_efgzfj.png",
+        "https://res.cloudinary.com/thlvsuvu/image/upload/f_auto,q_auto,w_2560/v1789363252/koppermaximusarg-5_wxremc.png",
         {
             imagen: "http://imgfz.com/i/w35OH6B.png",
             nsfw: 1
         }
-        "https://res.cloudinary.com/thlvsuvu/image/upload/f_auto,q_auto,w_2560/v1789363252/koppermaximusarg-5_wxremc.png",
     ],
     
     
